e5c2b59c553cfbee6acdac4065460abc3f389933
David Carek <david.carek@hyland.com>
2018-04-02 16:16:47 -0400

    added firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+        apiKey: 'AIzaSyBru0h0_vsZT8C-VdqZrzoCHP1A3g17p_M',
+        authDomain: 'fireapp-4693f.firebaseapp.com',
+        databaseURL: 'https://fireapp-4693f.firebaseio.com',
+        projectId: 'fireapp-4693f',
+        storageBucket: 'fireapp-4693f.appspot.com',
+        messagingSenderId: '1077640845523'
+  }
 };
